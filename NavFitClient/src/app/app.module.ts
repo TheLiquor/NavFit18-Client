40bd4f9d51f136629195d1e62a585c434795e4e9
Joe Fowler <fowlerjg90@gmail.com>
2018-10-07 14:20:02 -0500

    Added in different pages to begin work on client

---
+++ NavFitClient/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { PersonnelPage } from '../pages/personnel/personnel';
+import { EvalPage } from '../pages/eval/eval';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    HomePage,
+    AboutPage,
+    PersonnelPage,
+    EvalPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,
+    HomePage,
+    AboutPage,
+    PersonnelPage,
+    EvalPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
